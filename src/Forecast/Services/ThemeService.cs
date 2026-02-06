18c48b2ff2a4c34cf743fdb11d5f3a0da8a0b22e
Aleksandr Shastun <autour89@gmail.com>
2026-02-06 11:44:45 +0200

    Updated service.

--- src/Forecast/Services/ThemeService.cs
+++ src/Forecast/Services/ThemeService.cs
@@ -6,17 +6,10 @@ public class ThemeService : IThemeService
 {
     public void SetTheme(bool isDarkTheme)
     {
-        try
+        MainThread.BeginInvokeOnMainThread(() =>
         {
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
-                Application.Current?.UserAppTheme = isDarkTheme ? AppTheme.Dark : AppTheme.Light;
-            });
-        }
-        catch (Exception ex)
-        {
-            System.Diagnostics.Debug.WriteLine($"Error applying theme: {ex.Message}");
-        }
+            Application.Current?.UserAppTheme = isDarkTheme ? AppTheme.Dark : AppTheme.Light;
+        });
     }
 
     public bool IsDark() => Application.Current?.UserAppTheme == AppTheme.Dark;
